25c3a78400dcdcba5aa0aebb89b8fcb4ac3671cc
Carlos A. Marquez <carlos.marquez@microsoft.com>
2020-12-03 23:16:08 -0800

    Update customscript  to include Azure CLI and Helm

--- content/labs/scripts/customscript.sh
+++ content/labs/scripts/customscript.sh
@@ -1,1 +1,8 @@
+# Install .NET 5.0
 sudo snap install dotnet-sdk --classic
+
+# Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install HELM 3
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
