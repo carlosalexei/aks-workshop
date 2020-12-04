3a689334de27891e026c17a8198313241ac6a5cf
Carlos Alexei Marquez <carlosmr@microsoft.com>
2020-12-04 01:11:09 -0800

    Update customscript - logging

--- content/labs/scripts/customscript.sh
+++ content/labs/scripts/customscript.sh
@@ -1,8 +1,11 @@
 # Install .NET 5.0
-sudo snap install dotnet-sdk --classic
+echo ******* Installing .NET 5.0 >> events.log
+sudo snap install dotnet-sdk --classic >> events.log 
 
 # Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo ******* Installing Azure CLI >> events.log
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash >> events.log
 
 # Install HELM 3
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+echo ******* Installing Helm >> events.log
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash >> events.log
